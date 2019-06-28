23a1bd25787b16398a55ba4326063585333ea44d
Egojose <josemicrobiologo@gmail.com>
2019-06-27 22:54:17 -0500

    se actualiza module con nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxCurrencyModule } from "ngx-currency";
 import { OrdenServiciosComponent } from './orden-servicios/orden-servicios.component';
+import { MatExpansionModule, MatFormFieldModule, MatInputModule, MatCardModule, MatRadioModule, MatSlideToggleModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule, MAT_DATE_FORMATS, DateAdapter, MAT_DATE_LOCALE, MatTableModule, MatPaginatorModule, MatToolbarModule } from '@angular/material';
+// import { BsModalService } from 'ngx-bootstrap/modal/public_api';
+import { SPServicio } from './servicios/sp-servicio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -14,9 +19,27 @@ import { OrdenServiciosComponent } from './orden-servicios/orden-servicios.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxCurrencyModule
+    NgxCurrencyModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule, 
+    MatPaginatorModule, 
+    MatToolbarModule,
+    MatCheckboxModule
   ],
-  providers: [],
+  providers: [
+    SPServicio,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
