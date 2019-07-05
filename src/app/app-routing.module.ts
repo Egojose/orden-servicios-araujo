7a24158da5f728b635ae7d78f9c2162d1f2fbd9c
Egojose <josemicrobiologo@gmail.com>
2019-07-05 10:40:57 -0500

    se agregan las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrdenServiciosComponent } from './orden-servicios/orden-servicios.component';
+import { AprobarOrdenServicioComponent } from './aprobar-orden-servicio/aprobar-orden-servicio.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', redirectTo: 'app-root', pathMatch: 'full'},
+  {path:'orden-servicios', component:OrdenServiciosComponent},
+  {path:'aprobar-orden-servicio', component:AprobarOrdenServicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
