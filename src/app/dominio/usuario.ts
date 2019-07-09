e1db5e2d922e7c951c7e46d7a3caac9270ab6545
Egojose <josemicrobiologo@gmail.com>
2019-07-09 17:55:21 -0500

    se agrega info jefe a dominio usuario

--- src/app/dominio/usuario.ts
+++ src/app/dominio/usuario.ts
@@ -3,15 +3,23 @@ export class Usuario{
     id: number;
     nombre: string;
     email: string;
+    IdJefeDirecto: number;
+    NombreJefeDirecto: string;
+    Area: string;
+    EmailJefeDirecto: string;    
 
-    constructor(nombre: string, email: string, id: number){
+    constructor(nombre: string, email: string, id: number, IdJefeDirecto?: number, NombreJefeDirecto?: string, Area?: string, EmailJefeDirecto?: string){
         this.id = id;
         this.nombre = nombre;
         this.email = email;
+        this.IdJefeDirecto= IdJefeDirecto;
+        this.NombreJefeDirecto = NombreJefeDirecto;
+        this.Area = Area;
+        this.EmailJefeDirecto = EmailJefeDirecto;
     }
 
     public static fromJson(element: any) {
-        return new Usuario(element.Title, element.Email, element.Id);
+        return new Usuario(element.Title, element.Email, element.Id, -1, "", "", "");
     }
 
     public static fromJsonList(elements: any) {
