a22d9368f5779fd11654231a368c43dd30aaf71e
Egojose <josemicrobiologo@gmail.com>
2019-07-09 17:49:49 -0500

    se agrega ruta a consultar orden

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrdenServiciosComponent } from './orden-servicios/orden-servicios.component';
 import { AprobarOrdenServicioComponent } from './aprobar-orden-servicio/aprobar-orden-servicio.component';
-
+import { ConsultarOrdenComponent } from './consultar-orden/consultar-orden.component'; 
 
 const routes: Routes = [
-  {path:'', redirectTo: 'app-root', pathMatch: 'full'},
+  {path:'', redirectTo: 'orden-servicios', pathMatch: 'full'},
   {path:'orden-servicios', component:OrdenServiciosComponent},
-  {path:'aprobar-orden-servicio', component:AprobarOrdenServicioComponent}
+  {path:'aprobar-orden-servicio', component:AprobarOrdenServicioComponent},
+  {path:'consultar-orden', component:ConsultarOrdenComponent}
 ];
 
 @NgModule({
