c00030d490cf0952f1af96ed0ad501fb617de5a3
Egojose <josemicrobiologo@gmail.com>
2019-07-18 19:05:44 -0500

    se agrega componente bandeja de pendientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { OrdenServiciosComponent } from './orden-servicios/orden-servicios.component';
 import { AprobarOrdenServicioComponent } from './aprobar-orden-servicio/aprobar-orden-servicio.component';
 import { ConsultarOrdenComponent } from './consultar-orden/consultar-orden.component'; 
+import { BandejaServiciosComponent } from './bandeja-servicios/bandeja-servicios.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'orden-servicios', pathMatch: 'full'},
   {path:'orden-servicios', component:OrdenServiciosComponent},
   {path:'aprobar-orden-servicio', component:AprobarOrdenServicioComponent},
-  {path:'consultar-orden', component:ConsultarOrdenComponent}
+  {path:'consultar-orden', component:ConsultarOrdenComponent},
+  {path:'bandeja-servicios', component:BandejaServiciosComponent}
 ];
 
 @NgModule({
---
+++ src/app/dominio/BandejaServicios.ts
@@ -0,0 +1,29 @@
+export class BandejaServicios{
+    constructor(
+        public Id: string,
+        public NumeroOrden: string,
+        public Solicitante: string,
+        public FechaSolicitud: Date,
+        public Empresa: string,
+        public Estado: string
+    ) { }
+
+    public static fromJson(element: any) {
+        return new BandejaServicios(
+            element.Id,
+            element.NroOrden,
+            element.UsuarioSolicitante.Title,
+            element.Created,
+            element.Title,
+            element.Estado
+        );
+    }
+
+    public static fromJsonList(elements: any) {
+        var list = [];
+        for (var i = 0; i < elements.length; i++) {
+            list.push(this.fromJson(elements[i]));
+        }
+        return list;
+    }
+}
