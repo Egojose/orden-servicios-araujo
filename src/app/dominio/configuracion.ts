05ee0f5d23f1ba3b6b539002703371940144682a
Egojose <josemicrobiologo@gmail.com>
2019-07-23 11:20:09 -0500

    se agrega id al dominio

--- src/app/dominio/configuracion.ts
+++ src/app/dominio/configuracion.ts
@@ -4,7 +4,8 @@ export class Configuracion{
         public consecutivoAsociados: string,
         public iva: number,
         public consultores: string,
-        public asociados: string) {}
+        public asociados: string,
+        public id: number) {}
 
     public static fromJson(element: any) {
         return new Configuracion(
@@ -12,7 +13,8 @@ export class Configuracion{
             element.ConsecutivoAsociados,
             element.valorIva,
             element.AraujoConsultores,
-            element.AraujoAsociados);
+            element.AraujoAsociados,
+            element.Id);
     }
 
     public static fromJsonList(elements: any) {
