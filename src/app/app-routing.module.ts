5ffae46800457e8f81ce1b7d00566b7f342a097c
Egojose <josemicrobiologo@gmail.com>
2019-07-29 17:04:29 -0500

    se creo componente editar orden

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { OrdenServiciosComponent } from './orden-servicios/orden-servicios.compo
 import { AprobarOrdenServicioComponent } from './aprobar-orden-servicio/aprobar-orden-servicio.component';
 import { ConsultarOrdenComponent } from './consultar-orden/consultar-orden.component'; 
 import { BandejaServiciosComponent } from './bandeja-servicios/bandeja-servicios.component';
+import { EditarOrdenComponent } from './editar-orden/editar-orden.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'orden-servicios', pathMatch: 'full'},
   {path:'orden-servicios', component:OrdenServiciosComponent},
   {path:'aprobar-orden-servicio', component:AprobarOrdenServicioComponent},
   {path:'consultar-orden', component:ConsultarOrdenComponent},
-  {path:'bandeja-servicios', component:BandejaServiciosComponent}
+  {path:'bandeja-servicios', component:BandejaServiciosComponent},
+  {path:'editar-orden', component:EditarOrdenComponent}
 ];
 
 @NgModule({
---
+++ src/app/editar-orden/editar-orden.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-orden',
+  templateUrl: './editar-orden.component.html',
+  styleUrls: ['./editar-orden.component.css']
+})
+export class EditarOrdenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
