09ad146575e14805490769bd96ee1515f8a655b7
Egojose <josemicrobiologo@gmail.com>
2019-07-31 10:47:06 -0500

    se agrega tipo de consecutivo a consulta

--- src/app/dominio/empresas.ts
+++ src/app/dominio/empresas.ts
@@ -1,8 +1,8 @@
 export class Empresas{
-    constructor(public nombre: string, public id: number, public nit: string) {}
+    constructor(public nombre: string, public id: number, public nit: string, public tipo: string) {}
 
     public static fromJson(element: any) {
-        return new Empresas(element.RazonSocial, element.ID, element.Nit);
+        return new Empresas(element.RazonSocial, element.ID, element.Nit, element.TipoConsecutivo);
     }
 
     public static fromJsonList(elements: any) {
