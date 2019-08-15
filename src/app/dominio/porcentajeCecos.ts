6b12818dcb16f3b9abeea18a2a3572b893199b7c
Egojose <josemicrobiologo@gmail.com>
2019-08-15 15:48:33 -0500

    se modifican elementos en consulta

--- src/app/dominio/porcentajeCecos.ts
+++ src/app/dominio/porcentajeCecos.ts
@@ -3,11 +3,29 @@ export class PorcentajeCecos{
         public ceco: string, 
         public nombre: string, 
         public id: number, 
-        public nit: string, 
-        public tipo: string) {}
+        public porcentaje: string, 
+        public OrdenServicio: string,
+        public director: any,
+        public directorEmail: any,
+        public aprobado: boolean,
+        public fechaAprobado: string,
+        public firma: any
+       ) {}
 
     public static fromJson(element: any) {
-        return new PorcentajeCecos(element.Ceco, element.Nombre, element.ID, element.Nit, element.TipoConsecutivo);
+        let url; 
+        element.FirmaCeco !== null ? url = element.FirmaCeco.Url : url === "";
+        return new PorcentajeCecos(
+             element.Ceco,
+             element.Nombre, 
+             element.ID, 
+             element.PorcentajeAsumido, 
+             element.OrdenServicioId, 
+             element.DirectorCeco, 
+             element.DirectorCeco.EMail, 
+             element.Aprobado, 
+             element.FechaAprobacion, 
+             url);
     }
 
     public static fromJsonList(elements: any) {
